595b9ab406e0b67f71bea70f11fec07737997d5e
이로운 <lru040409@gmail.com>
2022-06-22 15:18:00 +0900

    Initial Commit

---
+++ PhotoDiary/PhotoDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDiaryApp.swift
+//  PhotoDiary
+//
+//  Created by 이로운 on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
